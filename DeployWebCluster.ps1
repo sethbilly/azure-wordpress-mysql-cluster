a02c86be41480e9f790bf78bb30f365c0aad1729
Tema Methodist Diocese <sethbilly.nartey@gmail.com>
2019-12-11 17:16:11 +0000

    Add PowerShell deployment script

---
+++ DeployWebCluster.ps1
@@ -0,0 +1,5 @@
+$GitBasePath = '/Users/billy/workspace/dev/demos/azure/AzureWordPressCluster'
+
+New-AzResourceGroupDeployment -ResourceGroupName  WordPressClusterGroup`
+    -TemplateFile "$GitBasePath/azuredeploy.json" `
+    -TemplateParameterFile "$GitBasePath/azuredeploy.parameters.json"
