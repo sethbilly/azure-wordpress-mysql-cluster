8676e57d6f9749d65688a8812a6872af480ab7be
Tema Methodist Diocese <sethbilly.nartey@gmail.com>
2019-12-12 09:05:39 +0000

    Change schema version to 2015-01-01

--- DeployWebCluster.ps1
+++ DeployWebCluster.ps1
@@ -1,5 +1,5 @@
 $GitBasePath = '/Users/billy/workspace/dev/demos/azure/AzureWordPressCluster'
 
-New-AzResourceGroupDeployment -ResourceGroupName  WordPressClusterGroup`
+New-AzResourceGroupDeployment -ResourceGroupName  WordPressClusterGroup `
     -TemplateFile "$GitBasePath/azuredeploy.json" `
     -TemplateParameterFile "$GitBasePath/azuredeploy.parameters.json"
